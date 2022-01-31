129b4d4e87e11b98864fbac772cbbf68a7d20a97
Dendie <dendie.sanjaya@gmail.com>
2022-01-31 11:00:28 +0700

    inisialisasi file project laravel

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ColorGrade;
+use App\Models\ColorList;
+use App\Models\ColorPattern;
+use App\Models\Config;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Session;
+
+class DashboardController extends BaseController
+{
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	public function index()
+	{		
+
+	   $data = array();	
+	   $config = Config::where('is_delete','no')->get();
+
+	   return view('dashboard.index',['data' => $data]);
+	}    
+}
