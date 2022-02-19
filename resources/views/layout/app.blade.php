d7c329a252ecc649fe14b7702afffbfb7f7d8852
Dendie <dendie.sanjaya@gmail.com>
2022-02-19 22:17:02 +0700

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,13 +20,17 @@
 <body class="theme-dark" data-background="none" data-highlight="red2">
         
 <div id="page">
-        
+
     <div id="page-preloader">
         <div class="loader-main"><div class="preload-spinner border-highlight"></div></div>
     </div>
 
 
     <div class="header header-fixed header-logo-center">
+        <div id="status">
+            status
+        </div>    
+
         <a href="{{ url('dashboard') }}" class="header-title">Color Meter Integria</a>
         <a href="#" class="back-button header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
         <a href="#" data-toggle-theme-switch class="header-icon header-icon-4"><i class="fas fa-question"></i></a>
@@ -51,12 +55,18 @@
 <script type="text/javascript" src="{{ url('scripts/plugins.js') }}"></script>
 <script type="text/javascript" src="{{ url('scripts/custom.js') }}"></script>
 <script type="text/javascript">
-/*
-console.log('Initially ' + (window.navigator.onLine ? 'on' : 'off') + 'line');
-window.addEventListener('online', () => console.log('Became online'));
-window.addEventListener('offline', () => console.log('Became offline'));
-document.getElementById('statusCheck').addEventListener('click', () => console.log('window.navigator.onLine is ' + window.navigator.onLine));
-*/
+
+window.addEventListener("offline", (event) => {
+  const statusDisplay = document.getElementById("status");
+  alert('OFFline');
+  statusDisplay.textContent = "OFFline";
+});
+
+window.addEventListener("online", (event) => {
+  const statusDisplay = document.getElementById("status");
+  alert('Online');
+  statusDisplay.textContent = "Online";
+});
 </script>
 
 @yield('js')
